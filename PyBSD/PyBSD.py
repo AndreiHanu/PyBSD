c77d675d73a05a5930d481b7f8969360c64edc9f
Andrei Hanu <hanua@mcmaster.ca>
2017-10-26 10:09:51 -0400

    Added function to plot the migration matrix from a PyBSD class object

--- PyBSD/PyBSD.py
+++ PyBSD/PyBSD.py
@@ -7,6 +7,11 @@ from matplotlib import pyplot as plt
 from matplotlib import rcParams
 from matplotlib import colors
 
+# Color palette library for Python
+# How to choose a colour scheme for your data:
+# http://earthobservatory.nasa.gov/blogs/elegantfigures/2013/08/05/subtleties-of-color-part-1-of-6/
+import palettable
+
 ##########################################################################################
 # Setting rcParams for publication quality graphs
 fig_width_pt = 246.0                    # Get this from LaTeX using \showthe\columnwidth
@@ -47,10 +52,11 @@ class PyBSD(object):
         self.migration = migration
         self.truth = truth
 
+    # Function to plot the measure data histogram
     def plotData(self, fName, withErrors = False):
         # Check the data type is a TH1 instance
-        if not isinstance(self.data, ROOT.TH1):
-            raise TypeError("Data must be an instance of ROOT.TH1")
+        #if not isinstance(self.data, ROOT.TH1):
+        #    raise TypeError("Data must be an instance of ROOT.TH1")
 
         # Get bin values, errors, and edges
         binVal = np.array([self.data.GetBinContent(i+1) for i in range(0, self.data.GetNbinsX())])
@@ -73,7 +79,7 @@ class PyBSD(object):
         if withErrors:
             axData.errorbar(binCenter,binVal,yerr=binValErr,capsize=0,ls='none',elinewidth=1.25, fmt='-')
 
-        # Figure Properties
+        # Figure properties
         axData.set_xlabel('True Energy (keV)' if not self.data.GetXaxis().GetTitle() else self.data.GetXaxis().GetTitle())
         axData.set_ylabel('# of Events' if not self.data.GetYaxis().GetTitle() else self.data.GetYaxis().GetTitle())
         axData.set_xlim(min(binEdge),max(binEdge))
@@ -90,6 +96,54 @@ class PyBSD(object):
 
         # Show the figure
         plt.close(figData)
+    
+    # Function to plot the migration matrix
+    def plotMigration(self, fName):
+        # Check the migration matrix type is of TH2 instance
+        #if not isinstance(self.migration, ROOT.TH2):
+        #    raise TypeError("Migration must be an instance of ROOT.TH2")
+
+        # Get bin values, errors, and edges
+        binVal = np.array([[self.migration.GetBinContent(i+1,j+1) for i in range(0, self.migration.GetNbinsX())] for j in range(0, self.migration.GetNbinsY())])
+        binEdge = np.array([[self.migration.GetXaxis().GetBinLowEdge(i+1) for i in range(0, self.migration.GetNbinsX() + 1)],
+                            [self.migration.GetYaxis().GetBinLowEdge(i+1) for i in range(0, self.migration.GetNbinsY() + 1)]])
+
+        # Create a figure
+        figMigration, axMigration = plt.subplots()
+
+        # Color map
+        cmap = palettable.matplotlib.Viridis_20.mpl_colormap
+        cmap.set_bad(cmap(0.))
+        cmap.set_over(cmap(1.)) 
+
+        # Plot the migration matrix
+        X, Y = np.meshgrid(binEdge[0], binEdge[1])
+        H = axMigration.pcolormesh(X, Y, binVal, norm = colors.LogNorm(), cmap = cmap, linewidth = 0, rasterized = True)
+
+        # Set color limits for the plot
+        H.set_clim(np.power(10, np.floor(np.log10(np.min(binVal[binVal>0])))), np.power(10, np.ceil(np.log10(np.max(binVal[binVal>0])))))
+
+        # Add a colorbar
+        cbar = figMigration.colorbar(H, ax=axMigration, pad = 0.01, aspect = 20., extend = 'both', spacing = 'uniform')
+        cbar.set_label('# of Events' if not self.migration.GetZaxis().GetTitle() else self.migration.GetZaxis().GetTitle())  
+
+        # Figure properties
+        axMigration.set_xlabel('Measured Energy (keV)' if not self.migration.GetXaxis().GetTitle() else self.migration.GetXaxis().GetTitle())
+        axMigration.set_ylabel('True Energy (keV)' if not self.migration.GetYaxis().GetTitle() else self.migration.GetYaxis().GetTitle())
+        axMigration.set_xlim(min(binEdge[0]),max(binEdge[0]))
+        axMigration.set_ylim(min(binEdge[1]),max(binEdge[1]))
+        axMigration.set_xscale('log')
+        axMigration.set_yscale('log')
+
+        # Fine-tune figure 
+        figMigration.tight_layout()
+
+        # Save the figure 
+        plt.savefig(fName, bbox_inches="tight")
+        print 'Migration matrix plot saved to: ' + fName
+
+        # Show the figure
+        plt.close(figMigration)
 
 # Load the data from the ROOT file
 fData = ROOT.TFile.Open('./../TestData/electron_Exp_1000_keV_R_20_cm_Nr_200000000_ISO.root')
@@ -97,4 +151,6 @@ fData = ROOT.TFile.Open('./../TestData/electron_Exp_1000_keV_R_20_cm_Nr_20000000
 # Test the class
 myBSD = PyBSD()
 myBSD.data = fData.Get('Detector Measured Spectrum')
-myBSD.plotData('Test.jpg')
+myBSD.migration = fData.Get('Energy Migration Matrix (Electron)')
+myBSD.plotData('Data.jpg')
+myBSD.plotMigration('Migration.jpg')
